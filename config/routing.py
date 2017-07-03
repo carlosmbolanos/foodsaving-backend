10faab66c230fcedfb81fcb35f4fdd5241ef78be
Nick Sellen <git@nicksellen.co.uk>
2017-07-03 21:25:38 +0200

    Add proof of concept django channels

---
+++ config/routing.py
@@ -0,0 +1,7 @@
+from channels.routing import route
+from config.consumers import ws_connect, ws_message
+
+channel_routing = [
+    route("websocket.connect", ws_connect),
+    route("websocket.receive", ws_message),
+]
